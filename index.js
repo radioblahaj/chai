d7b54f957c557af15383d1532cc29cbee88e7d18
Arav Narula <arav.1tb@gmail.com>
2024-04-27 23:50:01 -0400

    prisma works now

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const { App, LogLevel } = require('@slack/bolt');
 require('dotenv').config()
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+
 
 
 const app = new App({
@@ -12,10 +16,7 @@ const app = new App({
   port: process.env.PORT || 3000
 });
 
-let milkInChai = false;
-let buyMilk = false;
-const market = "firehouse";
-let sugarLevel = 0; // Default
+
 
 // if (sugarLevel = 0) {
 //     const chaiMessage = "Woah, are you sure you want chai without sugar?"
@@ -28,30 +29,31 @@ let sugarLevel = 0; // Default
 
 
 // Listens to incoming messages that contain "hello"
-app.message('chai', async ({ message, say }) => {
+app.command('/start-story', async ({ message, say, client, ack, command }) => {
+  await ack();
 
-try {
-  const user = await app.client.users.info({ user: body.user.id })
-  await say(`${user}`)
-  // then...
 
-  // yes
-} catch (e) {
-  await say(`${e}`)
-}
-  
-  if(milkInChai = false) {
-    await say({text: "Do you want milk in your chai?", thread_ts: message.ts})
-    milkInChai = true
-  } else {
-    await say("You already have milk in your chai")
-  }
+const userID = command.user_id;
+
+// let's see if this worksß
+
+  await say(`ID: ${userID}`)  
+  const user = await prisma.story.create({
+    data: {
+      buyMilk: false,
+      visits: 1,
+      milkInChai: false,
+      sugarinChai: false,
+      id: userID
+    },
+  })
+
 
   // say() sends a message to the channel where the event was triggered
 
 });
 
-app.message("yes", async ({message, say}) => {
+app.message("yes", async ({ message, say }) => {
 
 
 
@@ -59,24 +61,24 @@ app.message("yes", async ({message, say}) => {
 })
 
 
-app.message('milk', async({message, say}) => {
-
-  
-    await say({ text: "I'm awake! ⭐️ Do you need assistance?", thread_ts: message.ts });
-  
+app.message('milk', async ({ message, say }) => {
 
 
-
-  
-        
+  await say({ text: "I'm awake! ⭐️ Do you need assistance?", thread_ts: message.ts });
 
 
-        await say("There isn't milk")
-        buyMilk = true;
-        if (buyMilk) {           
-            await say("Do you want to buy milk")
-        }
-      });
+
+
+
+
+
+
+  await say("There isn't milk")
+  buyMilk = true;
+  if (buyMilk) {
+    await say("Do you want to buy milk")
+  }
+});
 
 
 
