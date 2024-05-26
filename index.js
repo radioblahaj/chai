dd8104253db642708f9171e6215e340433063eb5
Arav Narula <arav.1tb@gmail.com>
2024-05-26 17:37:42 -0400

    reset milk when user resets

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-const { App, LogLevel } = require('@slack/bolt');
-require('dotenv').config()
-const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
-
-
-
+const { App, LogLevel } = require("@slack/bolt");
+require("dotenv").config();
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
@@ -13,23 +10,17 @@ const app = new App({
   appToken: process.env.SLACK_APP_TOKEN,
   // Socket Mode doesn't listen on a port, but in case you want your app to respond to OAuth,
   // you still need to listen on some port!
-  port: process.env.PORT || 3000
+  port: process.env.PORT || 3000,
 });
-
-
 
 // if (sugarLevel = 0) {
 //     const chaiMessage = "Woah, are you sure you want chai without sugar?"
 // }
 
-
-
 // something like
 
-
-
 // Listens to incoming messages that contain "hello"
-app.command('/start-story', async ({ message, say, client, ack, command }) => {
+app.command("/start-story", async ({ message, say, client, ack, command }) => {
   await ack();
 
   const userID = command.user_id;
@@ -42,125 +33,105 @@ app.command('/start-story', async ({ message, say, client, ack, command }) => {
       visits: 1,
       milkInChai: false,
       sugarinChai: false,
-      id: userID
+      id: userID,
     },
-  })
+  });
 
-  await say(`<@${command.user_id}> started a story`)
+  await say(`<@${command.user_id}> started a story`);
 });
 
-app.message('start', async ({ message, say, client, body }) => {
+app.message("start", async ({ message, say, client, body }) => {
   const userID = message.user;
 
-  await say("Thank you for visiting the cafe")
-
+  await say("Thank you for visiting the cafe");
 
   const user = await prisma.story.update({
     where: {
-     id: userID 
+      id: userID,
     },
     data: {
-      visits: 1
+      visits: 1,
     },
-  })
+  });
+});
 
-})
-
-app.command('/make-coffee', async ({message, say, client, ack, command}) => {
-  await ack()
+app.command("/make-coffee", async ({ message, say, client, ack, command }) => {
+  await ack();
 
   const userID = command.user_id;
 
-  await say(`<@${userID}> asked me to make coffee! I'm just a teapot, I can't make coffee. *418*`)
-})
+  await say(
+    `<@${userID}> asked me to make coffee! I'm just a teapot, I can't make coffee. *418*`,
+  );
+});
 
-
-
-app.command('/reset-story', async ({ message, say, client, ack, command }) => {
-  await ack()
+app.command("/reset-story", async ({ message, say, client, ack, command }) => {
+  await ack();
   const userID = command.user_id;
   try {
-    client.chat.postEphemeral({                          // <--- and here
+    client.chat.postEphemeral({
       channel: command.channel_id,
       user: userID,
-      text: "Your story has been reset"
+      text: "Your story has been reset",
     });
     const updateUser = await prisma.story.update({
       where: {
-        id: userID
+        id: userID,
       },
       data: {
-        visits: 0
+        visits: 0,
+        milkInChai: false,
       },
-    })
+    });
   } catch (e) {
-    console.log(e)
-    client.chat.postEphemeral({
+    console.log(e);
+    client.chat.postMessage({
       token: process.env.SLACK_BOT_TOKEN,
       channel: command.channel_id,
       user: userID,
       text: `${e}`,
-    })
+    });
   }
-})
+});
 
-app.message('brew', ({message, say, client, body}) => {
-
+app.message("brew", ({ message, say, client, body }) => {
   const userID = message.user;
+});
 
-
-
-})
-
-
-app.message('milk', async ({ message, say, client, body }) => {
-
+app.message("milk", async ({ message, say, client, body }) => {
   const userID = message.user;
 
   const userData = await prisma.story.findUnique({
     where: {
-      id: userID
-    }
-  })
+      id: userID,
+    },
+  });
+
   switch (userData.milkInChai) {
     case false:
       const addMilk = await prisma.story.update({
         where: {
-          id: userID
+          id: userID,
         },
         data: {
           milkInChai: true,
-          visits: {increment: 1}
+          visits: { increment: 1 },
         },
-      })
+      });
+      await say({
+        text: "You added milk to your chai!",
+        thread_ts: message.ts,
+      });
       break;
     case true:
-      await say("test")
-
+      await say("You already have milk in your chai");
+      break;
   }
-  try {
-    const addMilk = await prisma.story.update({
-      where: {
-        id: userID
-      },
-      data: {
-        milkInChai: true,
-        visits: {increment: 1}
-      },
-    })
-  } catch (e) {
-    await say(e)
-  }
-
-  await say({ text: "You added milk to your chai!", thread_ts: message.ts });
-
-
 });
-
 
 (async () => {
   // Start your app
   await app.start();
 
-  console.log('⚡️ Bolt app is running!');
+  console.log("⚡️ Bolt app is running!");
 })();
--- utils/getStory.js
+++ utils/getStory.js
@@ -1,11 +1,16 @@
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
-async function getData(userID) {
-    const getUser = await prisma.story.findFirst({
+async function getData(userID, database_field) {
+    await prisma.story.update({
         where: {
-            id: userID
-        }
-
-    })
+          id: userID
+        },
+        data: {
+         database_field: true,
+          visits: {increment: 1}
+        },
+      })
 }
+
+module.exports(getData)
